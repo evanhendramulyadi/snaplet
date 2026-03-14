0a003ffe54d52c78ab1ff9f7757a838140eab0fe
evanhendramulyadi <evanhendra719@gmail.com>
2026-03-14 12:40:50 +0700

    Update project files

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const startBtn = document.getElementById('startBtn');
+
+    startBtn.addEventListener('click', () => {
+        alert('Memulai Photobooth...');
+        // Di sini kamu bisa tambahkan logika untuk membuka kamera atau pindah halaman
+    });
+});
