eac2c2d328f16aca680c21ea22c6318f4a56e4c3
evanhendramulyadi <evanhendra719@gmail.com>
2026-03-16 14:50:42 +0700

    Update project files

--- js/script.js
+++ js/script.js
@@ -205,7 +205,7 @@ const xPos = (canvas.width - photoW) / 2;
 const startY = 115;    
 
 // Jarak antar lubang (gap yang pas untuk frame ini cukup besar)
-const gap = 75;     
+const gap = 68;     
 
         // 3. Proses menggambar 4 foto satu per satu
         const promises = capturedPhotos.map((photoData, index) => {
